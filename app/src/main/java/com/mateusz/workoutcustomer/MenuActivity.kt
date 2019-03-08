c24b368915cd2853702752dc57c20821acac5e77
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2019-03-08 09:22:32 +0100

    Menu

--- app/src/main/java/com/mateusz/workoutcustomer/MenuActivity.kt
+++ app/src/main/java/com/mateusz/workoutcustomer/MenuActivity.kt
@@ -1,12 +1,38 @@
 package com.mateusz.workoutcustomer
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_menu.*
+import android.support.design.widget.TabLayout
+import android.support.v7.widget.Toolbar
 
 class MenuActivity : AppCompatActivity() {
+    val viewPager: ViewPager = findViewById(R.id.view_pager)
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_calendary -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_settings ->{
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
---
+++ app/src/main/java/com/mateusz/workoutcustomer/ScreenSlidePageFragment.kt
@@ -0,0 +1,16 @@
+package com.mateusz.workoutcustomer
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ScreenSlidePageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.activity_menu, container, false)
+}
